37d16a84c922c79c90d4cf8c6c73ef6b9d1fa09f
Brack Carmony <brackcarmony@gmail.com>
2016-08-10 16:48:01 -0600

    More comments

--- db/user_create_seed.sql
+++ db/user_create_seed.sql
@@ -1,4 +1,6 @@
--- It may be helpful to put
+-- It may be helpful to drop and reinstantilize the table when doing
+-- the tests in case you delete users/cars the tests are expecting to see
+-- DROP TABLE IF EXISTS users;
 
 ( 'John', 'Smith', 'John@Smith.com'),
 ( 'Dave', 'Davis', 'Dave@Davis.com'),
--- db/vehicle_create_seed.sql
+++ db/vehicle_create_seed.sql
@@ -1,3 +1,7 @@
+-- It may be helpful to drop and reinstantilize the table when doing
+-- the tests in case you delete users/cars the tests are expecting to see
+-- DROP TABLE IF EXISTS vehicles;
+
 ('Toyota', 'Camry', 1991, 1),
 ('Honda', 'Civic', 1995, 1),
 ('Ford', 'Focus', 2005, 1),
